df8e64d4359c79a43b420cc83eecd119897c5f43
Eman-Peralta <dev.emanperalta@gmail.com>
2025-07-05 18:01:52 +0800

    add p assive for swipe js

--- js/swipeContainer.js
+++ js/swipeContainer.js
@@ -30,15 +30,15 @@ swipeContainers.forEach(container => {
     container.scrollLeft = scrollLeft - walk;
   });
 
-  // Optional: Touch support for mobile
+  // Touch support for mobile
   container.addEventListener('touchstart', (e) => {
     startX = e.touches[0].pageX - container.offsetLeft;
     scrollLeft = container.scrollLeft;
-  });
+  }, { passive: true });
 
   container.addEventListener('touchmove', (e) => {
     const x = e.touches[0].pageX - container.offsetLeft;
     const walk = (x - startX) * 2;
     container.scrollLeft = scrollLeft - walk;
-  });
+  }, { passive: true });
 });
